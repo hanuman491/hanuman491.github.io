49fd39c924b106620f0f341e046ba2ebbb6ac06c
hanuman <hanuman.pasam@gmail.com>
2020-01-27 00:15:26 +0530

    live deployment

--- styles.js
+++ styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Resume\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! D:\MEAN\Angular-app\src\styles.css */"./src/styles.css");
 
 
 /***/ })
